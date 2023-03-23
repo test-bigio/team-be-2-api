683aede94c68b7c18826ffd507ee0151c517b66d
Purwa Darozatun <purwadarozatunakis@gmail.com>
2023-03-23 08:24:49 +0700

    comment ununsed

--- BigioHrServices/Program.cs
+++ BigioHrServices/Program.cs
@@ -15,13 +15,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped(typeof(IEmployeeService), typeof(EmployeeServices));
-builder.Services.AddScoped(typeof(INotificationService), typeof(NotificationServices));
-builder.Services.AddScoped(typeof(IDelegationMatrixService), typeof(DelegationMatrixServices));
-builder.Services.AddScoped(typeof(ILeaveApplicationService), typeof(LeaveApplicationServices));
-builder.Services.AddScoped(typeof(IPinSignatureService), typeof(PinSignatureService));
-builder.Services.AddScoped(typeof(IPositionService), typeof(PositionServices));
-builder.Services.AddScoped(typeof(IAuditLogService), typeof(AuditLogService));
+// builder.Services.AddScoped(typeof(IEmployeeService), typeof(EmployeeServices));
+// builder.Services.AddScoped(typeof(INotificationService), typeof(NotificationServices));
+// builder.Services.AddScoped(typeof(IDelegationMatrixService), typeof(DelegationMatrixServices));
+// builder.Services.AddScoped(typeof(ILeaveApplicationService), typeof(LeaveApplicationServices));
+// builder.Services.AddScoped(typeof(IPinSignatureService), typeof(PinSignatureService));
+// builder.Services.AddScoped(typeof(IPositionService), typeof(PositionServices));
+// builder.Services.AddScoped(typeof(IAuditLogService), typeof(AuditLogService));
 
 var app = builder.Build();
 
